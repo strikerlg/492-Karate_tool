2390213b991fece7047b443ae0327fd59b5df218
Zachariah Alber <zachariah.man.of.god@gmail.com>
2017-01-25 00:51:49 -0500

    Major css
    made some major font and color changes

--- html/php/tkakatagrab.php
+++ html/php/tkakatagrab.php
@@ -1,38 +1,16 @@
 <?php
 
+$dsn = 'mysql:host=107.147.40.137;port=80;dbname=TKACONT';
+$user = 'root';
+$password = 'mickey456';
+$dbh = new PDO($dsn, $user, $password);
 
-$con = mysqli_connect('107.147.40.137','root','mickey456','TKACONT');
-//if(!$con){
-//    die('Could not connect: ' . mysqli_error($con));
-//}
+$sth = $dbh->prepare("SELECT Firstname, Lastname, Kata FROM Contestant");
+$sth->execute();
 
-//mysqli_select_db($con,"TKACONT");
-//$sql="SELECT Firstname,Lastname,Kata FROM Contestant ORDER BY Kata DESC";
-//$result = mysqli_query($con,$sql);
+$result = $sth->fetchAll();
 
-//echo '<response>';
-//echo "<table>
-//<tr>
-//<th>Points</th>
-//<th>Firstname</th>
-//<th>Lastname</th>";
-////while loop right here
-//while($row = mysqli_fetch_array($result))
-//{
-//    echo "<tr>";
-//    echo "<td>" . $row['Kata'] .  "</td>";
-//    echo "<td>" . $row['Firstname'] . "</td>";
-//    echo "<td>" . $row['Lastname'] .  "</td>";
-//    echo "</tr>";
-//}
-//
-//echo "</table>";
-//echo '</response>';
+echo json_encode($result);
 
-$result = mysql_query("SELECT Firstname,Lastname,Kata FROM Contestant ORDER BY Kata DESC",$con) or die (mysql_error());
-$resultArray = mysql_fetch_row($result);
-echo json_encode($value);
-
-
-mysqli_close($con);
 ?>
+
